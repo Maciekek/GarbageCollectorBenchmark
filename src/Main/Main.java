c0238e9278da0b63fd9a14576d7c3e2832cba9c2
Maciek <maciekek@gmail.com>
2015-03-19 23:32:55 +0100

    uncomment

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,93 +12,30 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-//        Test1TimeForNAlloc.count();
-//        System.out.println("----------------");
+        Test1TimeForNAlloc.count();
+        System.out.println("----------------");
 
-//        Test1CountAllocObjects.count();
-//        System.out.println("----------------");
+        Test1CountAllocObjects.count();
+        System.out.println("----------------");
 
-//        Test2TimeForNAlloc.count();
-//        System.out.println("----------------");
+        Test2TimeForNAlloc.count();
+        System.out.println("----------------");
 
-//        Test2CountAllocObjects.count();
-//        System.out.println("----------------");
-//
-//        Test3TimeForNAlloc.count();
-//        System.out.println("----------------");
-//
-//        Test3CountAllocObjects.count();
-//        System.out.println("----------------");
-//
+        Test2CountAllocObjects.count();
+        System.out.println("----------------");
+
+        Test3TimeForNAlloc.count();
+        System.out.println("----------------");
+
+        Test3CountAllocObjects.count();
+        System.out.println("----------------");
+
         Test4CountAllocObjects.count();
-//        System.out.println("----------------");
-//
-//        Test4TimeForNAlloc.count();
+        System.out.println("----------------");
+
+        Test4TimeForNAlloc.count();
 
 
-
-
-
-//        Long start = System.nanoTime();
-//        for(int i =0;i<10000;i++){
-//            MemoryEater.alloc(1);
-//        }
-//        Long end = System.nanoTime();
-//        System.out.println((end-start)/1000000000.0)
-
-
-//        new Thread(new Main()).start();
-//        Thread.sleep(1000);
-//        flag = false;
-//        System.out.println("Wynik: " + count);
-//
-//        count=1;
-//        flag = true;
-//        for (int i=0;i<4;i++){
-//            new Thread(new Main()).start();
-//        }
-//        Thread.sleep(1000);
-//        flag = false;
-//        Thread.sleep(1);
-//        System.out.println("Wynik: " + count);
-
-
-// test 2 test2
-//        long allTime = 0;
-//        for(int i =0;i<10;i++){
-//
-//            Test2 test2 = new Test2();
-//            allTime+=test2.Test2();
-//        }
-//        System.out.println("TIME: "+ allTime/10);
-//
-
-// test2 tsetInTime
-
-
-//        Test2 test2 = new Test2();
-//        test2.testInTime();
-//
-//        Test3 test3 = new Test3();
-//        test3.Test1();
-
-
-//        test3.test1InTime();
-//        Test4 test4 = new Test4();
-//        test4.Test4();
-
-//        Test2 tes2 = new Test2();
-//        tes2.testInTime();
-//        allTime=0;
-//        for(int i =0;i<10;i++){
-//
-//            allTime+=Test1.Test1();
-//        }
-//        System.out.println("TIME: "+ allTime/10);
-
-
-
-//        Test1.test1InTime();
 
 
 
